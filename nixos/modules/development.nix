579b7cc71ae9f3aaa822cf38a7381d7e35d5823a
RHroot <sten181104@gmail.com>
2026-01-09 18:10:29 +0530

    automated dev commit

--- nixos/modules/development.nix
+++ nixos/modules/development.nix
@@ -52,15 +52,13 @@
   };
   services.ollama = {
     enable = true;
-    package = pkgs.ollama-cuda;
+    package = pkgs.ollama;
     port = 11111;
     host = "127.0.0.2";
     home = "/home/${env.username}";
-    syncModels = {
-      enable = true;
-      models = [
-        "qwen2.5:7b-instruct-q4"
-      ];
-    };
+    syncModels = true;
+    loadModels = [
+      "qwen2.5:7b-instruct-q4"
+    ];
   };
 }
