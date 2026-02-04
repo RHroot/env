ad5228fb44f12a20aa1b87bed549f7e553ef7f9b
RHroot <sten181104@gmail.com>
2026-02-04 14:06:58 +0530

    automated dev commit

--- nixos/modules/development.nix
+++ nixos/modules/development.nix
@@ -48,6 +48,7 @@
 
     # === Utility tools ===
     unstable.neovide # GPU-accelerated Neovim GUI
+    vscodium # Open-source build of Visual Studio Code
     lazygit # Terminal UI for Git operations
   ];
   # === Neovim ===
