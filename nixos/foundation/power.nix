07b6f12d62e1faf0aa028f47608889be912f7130
Rishabh Haldiya <rishabhhaldiya18@proton.me>
2026-04-01 19:00:10 +0530

    automated dev commit

--- nixos/foundation/power.nix
+++ nixos/foundation/power.nix
@@ -48,7 +48,7 @@
           CPU_BOOST_ON_BAT = 0;
           CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
           CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-          PLATFORM_PROFILE_ON_AC = "balanced";
+          PLATFORM_PROFILE_ON_AC = "performance";
           PLATFORM_PROFILE_ON_BAT = "low-power";
           USB_AUTOSUSPEND = 1;
           USB_EXCLUDE_BTUSB = 1;
