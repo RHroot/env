45f1a64fee948e559f6378bd1847e5159691ebe7
Rishabh Haldiya <rishabhhaldiya18@proton.me>
2026-04-21 15:51:38 +0530

    changed from librewolf to firefox

--- nixos/modules/utility.nix
+++ nixos/modules/utility.nix
@@ -6,11 +6,42 @@
   environment.systemPackages = with pkgs; [
     steam
     evince
-    librewolf
     libreoffice-fresh
   ];
 
+  programs.firefox = {
+    enable = true;
+    package = pkgs.firefox-bin;
+    preferences = {
+      # Privacy & Performance
+      "toolkit.telemetry.enabled" = false;
+      "datareporting.healthreport.uploadEnabled" = false;
+      "datareporting.policy.dataSubmissionEnabled" = false;
+      "browser.crashReports.unsubmittedCheck.autoSubmit2" = false;
+      "privacy.resistFingerprinting" = false;
+      "privacy.trackingprotection.enabled" = true;
+      "privacy.trackingprotection.cryptomining.enabled" = true;
+      "privacy.trackingprotection.fingerprinting.enabled" = true;
+      "browser.newtabpage.activity-stream.showSponsored" = false;
+      "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+      "browser.pocket.enabled" = false;
+      "browser.tabs.crashReporting.sendReport" = false;
+      "browser.sessionstore.interval" = 15000;
+      "network.http.max-connections" = 256;
+      # Native Vertical Tabs (Firefox 130+)
+      "browser.tabs.verticalTabs.enabled" = true;
+      # Force Dark Theme
+      "browser.theme.toolbar-theme" = 0;
+      "browser.theme.content-theme" = 0;
+      "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+      "ui.systemUsesDarkTheme" = 1;
+      # For the annoying remember password
+      "signon.rememberSignons" = false;
+    };
+  };
+  
   environment.sessionVariables = {
+    MOZ_USE_XINPUT2 = "1";
     MOZ_ENABLE_WAYLAND = "1";
   };
 
