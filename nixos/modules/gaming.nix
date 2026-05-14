2c887fd02405b60fce640ce75489176a88d7abcf
Rishabh Haldiya <rishabhhaldiya18@proton.me>
2026-05-15 04:05:21 +0530

    automated dev commit

--- nixos/modules/gaming.nix
+++ nixos/modules/gaming.nix
@@ -6,14 +6,35 @@
 : {
   environment.systemPackages = with pkgs; [
     lutris # A simple, efficient and really customizable game launcher
-    mangohud # MangoHUD
-    gamescope # A launcher that optimizes the gaming experience
-    winetricks # A program for installing and managing wine
-    protonup-qt # A program for installing and managing wine
+    wineWowPackages.full # Full Wine package with all components
+    winetricks # For installing Wine components
+    protontricks # For installing Proton components
+    protonup-qt # For installing Proton components
+    mangohud # A FPS counter
+    gamemode # Automatically switches to gamemode when a game is running
+    dxvk # DirectX to Vulkan translation
+    vkd3d # DirectX 12 to Vulkan translation
+    cabextract # Required for winetricks to install some components
+    faudio # XAudio2 implementation for Wine
   ];
 
+  programs.gamemode.enable = true;
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+
+  services.flatpak.enable = true;
   # programs.steam = {
   #   enable = true;
-  #   package = pkgs.steam;
+  #   package = pkgs.steam.override {
+  #     extraEnv = {
+  #       OBS_VKCAPTURE = "1";
+  #       RADV_TEX_ANISO = "16";
+  #     };
+  #   };
+  #   extraCompatPackages = with pkgs; [
+  #     proton-ge-bin
+  #   ];
   # };
 }
