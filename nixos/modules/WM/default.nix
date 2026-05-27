1ede440136b292b5ade4b723cb7ccd6c985d8b6b
Rishabh Haldiya <rishabhhaldiya18@proton.me>
2026-05-27 10:15:06 +0530

    automated dev commit

--- nixos/modules/WM/default.nix
+++ nixos/modules/WM/default.nix
@@ -4,6 +4,6 @@
   ...
 }: {
   imports = [
-    ./hyprland.nix
+    ./qtile.nix
   ];
 }
---
+++ nixos/modules/WM/qtile.nix
@@ -0,0 +1,92 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  services.xserver = {
+    enable = true;
+    windowManager.qtile = {
+      enable = true;
+      # configFile = "/home/sten/env/.config/qtile/config.py";
+    };
+    displayManager.lightdm.enable = false;
+  };
+
+  services.picom = {
+    enable = true;
+    vSync = true;
+    backend = "glx";
+    fade = false;
+    shadow = false;
+  };
+
+  services.libinput = {
+    enable = true;
+
+    touchpad = {
+      disableWhileTyping = true;
+      tapping = true;
+      naturalScrolling = false;
+
+      clickMethod = "clickfinger"; # better multi-finger clicks
+      scrollMethod = "twofinger"; # standard
+      accelProfile = "flat"; # or "adaptive"
+      accelSpeed = "0.4"; # range: -1 to 1
+
+      middleEmulation = true; # 3-finger middle click
+    };
+
+    mouse = {
+      accelProfile = "flat";
+      accelSpeed = "0.0";
+      middleEmulation = true;
+    };
+  };
+
+  services.displayManager.gdm.enable = false;
+  services.displayManager.sddm.enable = false;
+
+  services.getty.autologinUser = "sten";
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.qtile}/bin/qtile";
+        user = "sten";
+      };
+    };
+  };
+
+  services.displayManager.defaultSession = "Qtile";
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+  };
+
+  environment.systemPackages = with pkgs; [
+    # === Qtile ===
+    qtile
+    picom
+
+    # === Basic ===
+    libinput # Input device management library
+    playerctl # Media player control via MPRIS
+    libnotify # Desktop notification library
+
+    # === Terminal emulators ===
+    kitty # Feature-rich GPU-based terminal emulator
+    alacritty # GPU-accelerated terminal emulator
+
+    # === File managers ===
+    nemo # File manager
+
+    # === Polkit ===
+    polkit_gnome # GNOME's polkit agent
+
+    # === XDG ===
+    xdg-utils # Desktop integration helpers (xdg-open, etc.)
+    xdg-desktop-portal # XDG desktop portal service
+  ];
+}
