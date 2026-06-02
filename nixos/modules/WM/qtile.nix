97ff15d406abd0832cd2fd4f981df31ff76cb0b7
Rishabh Haldiya <rishabhhaldiya18@proton.me>
2026-06-02 12:40:55 +0530

    automated dev commit

--- nixos/modules/WM/qtile.nix
+++ nixos/modules/WM/qtile.nix
@@ -92,6 +92,7 @@
     copyq # Clipboard manager
     kitty # Feature-rich GPU-based terminal emulator
     dunst # Desktop notifications
+    wallust # Color Pallete maker from Wallpapers
     nautilus # File manager
     libinput # Input device management library
     ristretto # Image viewer from XFCE
