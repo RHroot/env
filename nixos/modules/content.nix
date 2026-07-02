3e16721a5c98efe4619c3d96c634a0cfa97150a9
Rishabh Haldiya <rishabhhaldiya18@proton.me>
2026-07-02 11:08:51 +0530

    automated dev commit

--- nixos/modules/content.nix
+++ nixos/modules/content.nix
@@ -6,9 +6,6 @@
 }: {
   environment.systemPackages = with pkgs; [
     gimp # Advanced image editor (GNU Image Manipulation Program)
-    lmms # Digital audio workstation for music production
-    krita # Professional digital painting and illustration software
-    inkscape # Vector graphics editor for creating and editing scalable graphics
     audacity # Free, open-source audio editor and recorder
     obs-studio # Open Broadcaster Software for video recording and live streaming
     kdePackages.kdenlive # Non-linear video editor for creating and editing videos
