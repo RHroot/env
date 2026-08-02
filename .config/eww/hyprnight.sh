d564e6caab167cbfc4f9f97fbb4de8b30de5e373
Rishabh Haldiya <rishabhhaldiya18@proton.me>
2026-08-02 06:58:58 +0530

    automated dev commit

--- .config/eww/hyprnight.sh
+++ .config/eww/hyprnight.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/sh
 
 TEMP=4000
+GAMMA=80
 
 case "$1" in
   toggle)
     if pgrep -x hyprsunset >/dev/null; then
       pkill -x hyprsunset
     else
-      hyprsunset -t $TEMP >/dev/null 2>&1 &
+      hyprsunset -t $TEMP -g $GAMMA >/dev/null 2>&1 &
       disown
     fi
     ;;
