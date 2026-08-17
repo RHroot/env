60e3ca8a8d7db7eb696a9c861d518fb3a9d24376
Rishabh Haldiya <rishabhhaldiya18@proton.me>
2026-08-17 19:38:56 +0530

    automated dev commit

--- nixos/foundation/network.nix
+++ nixos/foundation/network.nix
@@ -40,11 +40,13 @@
     settings.Resolve = {
       DNS = [
         "8.8.8.8#dns.google"
+        "1.1.1.1#cloudflare-dns.com"
         "2001:4860:4860::8888#dns.google"
+        "2606:4700:4700::1111#cloudflare-dns.com"
       ];
       FallbackDNS = [
-        "1.1.1.1#cloudflare-dns.com"
-        "2606:4700:4700::1111#cloudflare-dns.com"
+        "9.9.9.9#dns.quad9.net"
+        "2620:fe::fe#dns.quad9.net"
       ];
       Domains = "~.";
       DNSSEC = "allow-downgrade";
